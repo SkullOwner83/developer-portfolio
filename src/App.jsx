9ece7eaaaf8fcb30bea653d83a04a41fa83cb583
Javier Guzmán Cruz <skullowner83@gmail.com>
2024-09-29 22:17:48 -0600

    Structuring the header with developer info and nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import {Footer} from "./Components/Footer.jsx";
 import {Header} from "./Components/Header.jsx";
 import './Styles/Styles.scss';
 
@@ -7,13 +6,6 @@ function App() {
   return (
     <div>
       <Header/>
-      
-      <main>
-        <h1>SOBRE MI</h1>
-        <p>Soy una persona apasionada por la tecnología y las herramientas digitales, asi como el desarrollo de aplicaciones y videojuegos. Además, estoy en una constante búsqueda de nuevo conocimiento y desarrollo profesional.</p>
-      </main>
-
-      <Footer/>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,38 @@
 export const Header = () => {
     return (
         <header>
-            <p>JAVIER GUZMAN CRUZ</p>
-            <p>Desarrollador de software</p>
-            <button>Curriculum</button>
-            <button>Contacto</button>
+            <div className="Left-Container">
+                <div className="Info-Container">
+                    <div className="Photo-Container">
+
+                    </div>
+
+                    <div className="Biografy-Container">
+                        <div>
+                            <p className="Biografy-Name">JAVIER GUZMAN CRUZ</p>
+                            <p className="Biografy-Description">Desarrollador de software</p>
+                        </div>
+                        
+                        <div className="Buttons-Container">
+                            <button className="Convencional-Button">Curriculum</button>
+                            <button className="Outlined-Button">Contacto</button>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Navbar menu options */}
+                <ul>
+                    <li>Iniciar juego</li>
+                    <li>Acerca de mi</li>
+                    <li>Tecnologías</li>
+                    <li>Experiencia</li>
+                    <li>Proyectos</li>
+                </ul>
+            </div>
+
+            <div className="Right-Container">
+
+            </div>
         </header>
     )
 }
